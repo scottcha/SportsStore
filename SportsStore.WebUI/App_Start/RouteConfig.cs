b4a04507513d41c0ae111fbfdafa971d0defd095
Scott Chamberlin <scottcha@live.com>
2013-09-09 17:53:26 -0700

    Added domain model, paging, and unit tests

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new {Controller = "Product", action = "List"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using Moq;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+            /*
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+                {
+                    new Product{Name = "Football", Price = 25},
+                    new Product{Name = "Surf board", Price = 179},
+                    new Product{Name = "Running shoes", Price = 95}
+
+                }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+             * */
+        }
+    }
+}
