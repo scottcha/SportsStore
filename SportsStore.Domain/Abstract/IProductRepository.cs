8573fa8af60be74b1b27475b5eb404d91829aea2
Scott Chamberlin <scottcha@live.com>
2013-11-02 21:51:24 -0700

    Finished adding admin listing/editing

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -9,6 +9,10 @@ namespace SportsStore.Domain.Abstract
 {
     public interface IProductRepository
     {
-        IQueryable<Product> Products { get; } 
+        IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
     }
 }
